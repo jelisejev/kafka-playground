c585fab97211460551638c0a1369dbfc9efa4c68
pavel.yeliseev <jelisejev@@users.noreply.github.com>
2020-03-17 12:19:32 +0200

    add consumer and producer

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const kafka = require('kafka-node');
 const Consumer = kafka.Consumer;
-const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+const filesClient = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+const transactionClient = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
 
-const consumer = new Consumer(
-    client,
+const producer = new kafka.Producer(filesClient);
+
+const filesConsumer = new Consumer(
+    filesClient,
     [
         { topic: 'files' }
     ]);
 
-console.log('Listening to messages');
-consumer.on('message', (message) => {
-    console.log(message);
+const transactionConsumer = new Consumer(
+    transactionClient,
+    [
+        { topic: 'transactions' }
+    ]);
+
+producer.on('ready', function () {
+
+    console.log('Producer ready, listening to file messages');
+    filesConsumer.on('message', (message) => {
+        console.log(`new file: ${JSON.stringify(message)}`)
+        producer.send([{
+            topic: 'transactions',
+            messages: ['message body']
+        }], () => {})
+    });
+    filesConsumer.on('error', (message) => {
+        console.error(message);
+    });
 });
-consumer.on('error', (message) => {
+
+transactionConsumer.on('message', (message) => {
+    console.log(`new transaction: ${JSON.stringify(message)}`)
+});
+transactionConsumer.on('error', (message) => {
     console.error(message);
 });
